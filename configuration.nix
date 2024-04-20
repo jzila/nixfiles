92c6d9f797530aec8c3112927bb37c99af6841a4
John Zila <john@jzila.com>
2024-04-20 13:17:55 -0500

    configuration.nix: enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,10 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
